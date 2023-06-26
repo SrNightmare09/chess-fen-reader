191ce3231c24df0d93c44dd1fb4050cdc6a743a3
SrNightmare09 <srnightmare09@gmail.com>
2023-06-26 16:29:05 +0530

    feat: function can read complete position-only fen

--- src/read.js
+++ src/read.js
@@ -1,25 +1,45 @@
-var fen = "r1bk3r/";
+var fen = "4k2r/6r1/8/8/8/8/3R4/R3K3";
 
 const pieceRegex = '[bpqrknBPQRKN]'
 const numRegex = '[12345678]'
 
-let pieces = [];
+let pieces = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+];
 
 function resolvePieces() {
 
-    for (let i = 0; i < fen.length - 1; i++) {
+    var indexCount = 0;
+    var rank = 0;
 
-        if (pieceRegex.match(fen.charAt(i))) {
-            pieces.push(fen.charAt(i));
+    while (indexCount != fen.length) {
+
+        if (pieceRegex.match(fen.charAt(indexCount))) {
+            pieces[rank].push(fen.charAt(indexCount));
         }
 
-        if (numRegex.match(fen.charAt(i))) {
+        if (numRegex.match(fen.charAt(indexCount))) {
 
-            for (let k = 0; k < parseInt(fen.charAt(i)); k++) {
-                pieces.push(' ')
+            for (let k = 0; k < parseInt(fen.charAt(indexCount)); k++) {
+                pieces[rank].push(' ')
             }
 
         }
+
+        if (fen.charAt(indexCount) == '/') {
+            rank++;
+            indexCount++;
+            continue;
+        }
+
+        indexCount++;
 
     }
 
