a2c70aa9651decf7f8b98234e981be1a7ca68128
SrNightmare09 <srnightmare09@gmail.com>
2023-06-26 17:11:35 +0530

    feat: generate chess board

---
+++ src/generateBoard.js
@@ -0,0 +1,22 @@
+function generateBoard() {
+
+    const chessboard = document.getElementById('chessboard');
+
+    for(let rank = 0; rank < 8; rank++) {
+        for (let square = 0; square < 8; square++) {
+            var cell = document.createElement('div');
+            cell.className = 'cell';
+
+            // color squares alternatively
+            if ((rank + square) % 2 == 0) {
+                cell.style.backgroundColor = "#ffffff";
+            } else {
+                cell.style.backgroundColor = "#000000";
+            }
+
+            chessboard.appendChild(cell);
+        }
+    }
+}
+
+generateBoard();
