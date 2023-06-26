5ceab3c09f899edd3afc29acb3cbac564fa6d4ce
SrNightmare09 <srnightmare09@gmail.com>
2023-06-26 18:22:50 +0530

    feat: add func to return path based on piece char

--- src/generateBoard.js
+++ src/generateBoard.js
@@ -1,5 +1,7 @@
 function generateBoard() {
 
+    const position = resolvePieces();
+
     const chessboard = document.getElementById('chessboard');
 
     for(let rank = 0; rank < 8; rank++) {
@@ -14,8 +16,47 @@ function generateBoard() {
                 cell.style.backgroundColor = "#a16f5a";
             }
 
+            if (position[rank][square] != ' ') {
+                // console.log(`[${rank}, ${square}]`); // works
+                var piecePath = getPiece(position[rank][square]);
+
+                var piece = document.createElement('img');
+                piece.src = `../images/${piecePath}.svg`;
+                cell.appendChild(piece);
+
+            }
+
             chessboard.appendChild(cell);
         }
+    }
+
+    console.log(position)
+}
+
+function getPiece(char) {
+
+    const pieces = {
+        'k': 'king',
+        'q': 'queen',
+        'r': 'rook',
+        'n': 'knight',
+        'b': 'bishop',
+        'p': 'pawn'
+    };
+
+    let uppercase;
+    if (char === char.toUpperCase()) {
+        uppercase = true;
+    } else {
+        uppercase = false;
+    }
+    char = char.toLowerCase();
+    var piece = pieces[char];
+
+    if (uppercase) {
+        return `white/${piece}`;
+    } else {
+        return `black/${piece}`;
     }
 }
 
