f955866643ee38b71eadc3e7aedd9b8d52736f5c
Andrii Voronov <voronov.a.g@gmail.com>
2023-06-27 13:09:26 +0200

    moved getPiece to a different file;

--- src/generateBoard.js
+++ src/generateBoard.js
@@ -31,31 +31,4 @@ function generateBoard() {
     }
 }
 
-function getPiece(char) {
-
-    const pieces = {
-        'k': 'king',
-        'q': 'queen',
-        'r': 'rook',
-        'n': 'knight',
-        'b': 'bishop',
-        'p': 'pawn'
-    };
-
-    let uppercase;
-    if (char === char.toUpperCase()) {
-        uppercase = true;
-    } else {
-        uppercase = false;
-    }
-    char = char.toLowerCase();
-    var piece = pieces[char];
-
-    if (uppercase) {
-        return `white/${piece}`;
-    } else {
-        return `black/${piece}`;
-    }
-}
-
 generateBoard();
