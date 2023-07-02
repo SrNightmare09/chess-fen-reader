8f9d81ff503b00652ed8d59d2672f2f64a5e1e1a
Prashanth-Chandra <surekhadrk@gmail.com>
2023-07-02 15:34:37 +0530

    Feat : Add option to change board theme

--- src/generateBoard.js
+++ src/generateBoard.js
@@ -5,16 +5,26 @@ function generateBoard(fenString) {
     const position = resolvePieces(fenString);
     const chessboard = document.getElementById('chessboard');
 
+    const cellThemeColour = {
+        'brown': ['#b58863', '#f0d9b5'],
+        'green': ['#769656', '#eeeed2'],
+        'blue': ['#4b7399', '#eae9d2'],
+        'pink': ['#fcd8dd', '#ffffff'],
+    }
+
     for (let rank = 0; rank < 8; rank++) {
         for (let square = 0; square < 8; square++) {
             var cell = document.createElement('div');
             cell.className = 'cell';
 
+            var cellTheme = getCellTheme();
+            var cellColour = cellThemeColour[cellTheme];
+
             // color squares alternatively
             if ((rank + square) % 2 == 0) {
-                cell.style.backgroundColor = "#ecd3b9";
+                cell.style.backgroundColor = cellColour[0];
             } else {
-                cell.style.backgroundColor = "#a16f5a";
+                cell.style.backgroundColor = cellColour[1];
             }
 
             if (position[rank][square] != ' ') {
