6f1f69d09cc156dcc8e30f668263d9f9a706deba
Prashanth-Chandra <surekhadrk@gmail.com>
2023-07-03 19:58:22 +0530

    refactor: Removed blank spaces and added documentation

--- server/script.bat
+++ server/script.bat
@@ -1,16 +1,15 @@
 @echo off
 
-
-set REPO_URL=https://github.com/SrNightmare09/chess-fen-reader.git
-
+@REM Get current directory
 set CLONE_DIR=%CD%
 
-set DIR_NAME="%CD%\chess-fen-reader"
-
+@REM Clone repository
+set REPO_URL=https://github.com/SrNightmare09/chess-fen-reader.git
 git clone %REPO_URL%
-    
-cd %CLONE_DIR%\chess-fen-reader\src\
 
+@REM Navigate to the project directory
+cd %CLONE_DIR%\chess-fen-reader\src\
 set FILE_NAME="%CD%\index.html"
 
+@REM Open index.html in the default browser
 start "" %FILE_NAME%
