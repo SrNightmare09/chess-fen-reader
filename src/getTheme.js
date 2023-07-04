ef6bb82951ce79af63b347380403ea4f02b966dc
splenta222 <amirhoseinakbari202@gmail.com>
2023-07-05 00:55:58 +0330

    Feat: buttons UI improved

--- src/getTheme.js
+++ src/getTheme.js
@@ -1,25 +1,48 @@
-const selectedTheme = document.getElementById('piece-theme');
+const CBurnett_theme = document.getElementById('cburnett-theme');
+const doodle_theme = document.getElementById('doodle-theme');
+let piece_theme = "cburnett"
 
 function getTheme() {
-    return selectedTheme.value;
+    return piece_theme;
 }
 
-selectedTheme.onchange = () => {
-    getTheme();
-    currentArrangement = getCurrentFen();
-    generateBoard(currentArrangement);
-    checkPreviousState();
+CBurnett_theme.onclick = () => {
+    piece_theme = "cburnett";
+    callGenerateBoard();
+}
+doodle_theme.onclick = () => {
+    piece_theme = "doodle";
+    callGenerateBoard();
 }
 
-const selectedCellTheme = document.getElementById('board-theme');
+const brown_theme = document.getElementById('brown-theme');
+const green_theme = document.getElementById('green-theme');
+const blue_theme = document.getElementById('blue-theme');
+const pink_theme = document.getElementById('pink-theme');
+let theme = "brown";
 
 function getCellTheme() {
-    return selectedCellTheme.value;
+    return theme;
 }
 
-selectedCellTheme.onchange = () => {
-    getCellTheme();
-    currentArrangement = getCurrentFen();
+function callGenerateBoard() {
+    let currentArrangement = getCurrentFen();
     generateBoard(currentArrangement);
     checkPreviousState();
+}
+brown_theme.onclick = () => {
+    theme = "brown";
+    callGenerateBoard();
+}
+green_theme.onclick = () => {
+    theme = "green";
+    callGenerateBoard();
+}
+blue_theme.onclick = () => {
+    theme = "blue";
+    callGenerateBoard();
+}
+pink_theme.onclick = () => {
+    theme = "pink";
+    callGenerateBoard();
 }
